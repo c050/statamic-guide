5f62492d3af4b342be033832f2e8bd0714313de0
crisavendano <coavendano@uc.cl>
2024-02-07 17:14:12 -0300

    User: added Uuids inside model and preferences to json

--- guide-project/app/Models/User.php
+++ guide-project/app/Models/User.php
@@ -11,6 +11,7 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    use \Illuminate\Database\Eloquent\Concerns\HasUuids;
 
     /**
      * The attributes that are mass assignable.
@@ -40,5 +41,6 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'preferences' => 'json',
     ];
 }
